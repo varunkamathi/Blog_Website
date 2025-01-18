4fe240ce2c07a03746bfb004925484eddc02d17b
varunkamathi <varunkamathi522004@gmail.com>
2025-01-18 14:03:37 +0530

    create footer components

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Projects from './pages/Projects'
 import Signin from './pages/Signin'
 import SignUp from './pages/SignUp'
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 
 export default function App() {
@@ -23,6 +24,7 @@ export default function App() {
 
 
     </Routes>
+    <Footer />
     </BrowserRouter>
   )
 }
