5392986169f2b6c96249f11620fa4545cf7255ad
varunkamathi <varunkamathi522004@gmail.com>
2025-01-21 19:24:04 +0530

    create private route for dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
-import DashBorad from './pages/DashBoard'
+import Dashboard from './pages/DashBoard'
 import Projects from './pages/Projects'
 import Signin from './pages/Signin'
 import SignUp from './pages/SignUp'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import PrivateRoute from './components/PrivateRoute';
 
 
 export default function App() {
@@ -17,7 +18,9 @@ export default function App() {
     <Routes>
       <Route path='/' element={<Home/>} />
       <Route path='/about' element={<About/>} />
-      <Route path='/dashboard' element={<DashBorad/>} />
+      <Route element={<PrivateRoute />}>
+          <Route path='/dashboard' element={<Dashboard />} />
+      </Route>      
       <Route path='/projects' element={<Projects/>} />
       <Route path='/signin' element={<Signin/>} />
       <Route path='/signup' element={<SignUp/>} />
