10f6a56ad0fe7329cc495db8492d53e2a857e03f
varunkamathi <varunkamathi522004@gmail.com>
2025-01-21 19:16:20 +0530

    add theme functionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
-import DashBorad from './pages/DashBorad'
+import DashBorad from './pages/DashBoard'
 import Projects from './pages/Projects'
 import Signin from './pages/Signin'
 import SignUp from './pages/SignUp'
@@ -17,7 +17,7 @@ export default function App() {
     <Routes>
       <Route path='/' element={<Home/>} />
       <Route path='/about' element={<About/>} />
-      <Route path='/dashborad' element={<DashBorad/>} />
+      <Route path='/dashboard' element={<DashBorad/>} />
       <Route path='/projects' element={<Projects/>} />
       <Route path='/signin' element={<Signin/>} />
       <Route path='/signup' element={<SignUp/>} />
