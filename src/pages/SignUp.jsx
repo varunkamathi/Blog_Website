df86f168d342aae0ed7079252cd489caa8751e19
varunkamathi <varunkamathi522004@gmail.com>
2025-01-31 20:22:54 +0530

    fix bug

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -85,7 +85,7 @@ export default function SignUp() {
         </form>
         <div className='flex gap-2 text-sm mt-5'>
           <span>Have an account?</span>
-          <Link to='/signin' className='text-blue-500'>SignIn</Link>
+          <Link to='/sign-in' className='text-blue-500'>SignIn</Link>
         </div>
         {
           errorMessage && (
