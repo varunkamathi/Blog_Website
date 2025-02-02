a92726e2ef93805d21fd52f4131dda168e08a494
varunkamathi <varunkamathi522004@gmail.com>
2025-02-02 21:22:53 +0530

    major chnages in post and comment components or pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,43 @@
-import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
-import About from './pages/About'
-import Dashboard from './pages/DashBoard'
-import Projects from './pages/Projects'
-import Signin from './pages/Signin'
-import SignUp from './pages/SignUp'
-import Header from './components/Header'
-import Footer from './components/Footer'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import SignIn from './pages/SignIn';
+import Dashboard from './pages/Dashboard';
+import Projects from './pages/Projects';
+import SignUp from './pages/SignUp';
+import Header from './components/Header';
+import Footer from './components/Footer';
 import PrivateRoute from './components/PrivateRoute';
-import CreatePost from './pages/CreatePost'
-import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute'
-
+import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute';
+import CreatePost from './pages/CreatePost';
+import UpdatePost from './pages/UpdatePost';
+import PostPage from './pages/PostPage';
+import ScrollToTop from './components/ScrollToTop';
+import Search from './pages/Search';
 
 export default function App() {
   return (
     <BrowserRouter>
-    <Header/>
-    <Routes>
-      <Route path='/' element={<Home/>} />
-      <Route path='/about' element={<About/>} />
-      <Route element={<PrivateRoute />}>
+      <ScrollToTop />
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/search' element={<Search />} />
+        <Route element={<PrivateRoute />}>
           <Route path='/dashboard' element={<Dashboard />} />
-      </Route>
-      <Route element={<OnlyAdminPrivateRoute />}>
+        </Route>
+        <Route element={<OnlyAdminPrivateRoute />}>
           <Route path='/create-post' element={<CreatePost />} />
-        </Route>      
-      <Route path='/projects' element={<Projects/>} />
-      <Route path='/sign-in' element={<Signin/>} />
-      <Route path='/signup' element={<SignUp/>} />
+          <Route path='/update-post/:postId' element={<UpdatePost />} />
+        </Route>
 
-
-    </Routes>
-    <Footer />
+        <Route path='/projects' element={<Projects />} />
+        <Route path='/post/:postSlug' element={<PostPage />} />
+      </Routes>
+      <Footer />
     </BrowserRouter>
-  )
+  );
 }
--- src/pages/DashBoard.jsx
+++ src/pages/DashBoard.jsx
@@ -2,8 +2,12 @@ import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import DashSidebar from '../components/DashSidebar';
 import DashProfile from '../components/DashProfile';
+import DashPosts from '../components/DashPost';
+import DashUsers from '../components/DashUsers';
+import DashComments from '../components/DashComments';
+import DashboardComp from '../components/DashboardComp';
 
-export default function DashBorad() {
+export default function Dashboard() {
   const location = useLocation();
   const [tab, setTab] = useState('');
   useEffect(() => {
@@ -14,12 +18,21 @@ export default function DashBorad() {
     }
   }, [location.search]);
   return (
-    <div className='min-h-screen flex flex-col md:flex-row'> <div className='md:w-56'>
-    {/* Sidebar */}
-    <DashSidebar />
-  </div>
-  {/* profile... */}
-  {tab === 'profile' && <DashProfile />}
-  </div>
-  )
+    <div className='min-h-screen flex flex-col md:flex-row'>
+      <div className='md:w-56'>
+        {/* Sidebar */}
+        <DashSidebar />
+      </div>
+      {/* profile... */}
+      {tab === 'profile' && <DashProfile />}
+      {/* posts... */}
+      {tab === 'posts' && <DashPosts />}
+      {/* users */}
+      {tab === 'users' && <DashUsers />}
+      {/* comments  */}
+      {tab === 'comments' && <DashComments />}
+      {/* dashboard comp */}
+      {tab === 'dash' && <DashboardComp />}
+    </div>
+  );
 }
